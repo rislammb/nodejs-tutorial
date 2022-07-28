b07e10d108ae11a6bd0e0ac58fb1cde2077aa94c
unknown <rislammb@gmail.com>
2022-07-28 13:04:14 +0600

    start nodejs with new way

---
+++ people.js
@@ -0,0 +1,13 @@
+const people = ['musfiq', 'nayem', 'sabbir'];
+
+const a = 6;
+function test() {
+    console.log('test');
+}
+
+console.log(module);
+module.exports = {
+    people,
+    a,
+    test,
+};
